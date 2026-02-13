864f7fb9a4d59aeccd1a9b70e4512485fcbd31c0
Kalpatarusahoo <Kalpataru.sahoo@kalpitatechnologies.com>
2026-02-13 09:28:51 +0530

    Stop tracking environment files for security

--- src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  // This is your live API on Render
-  apiUrl: 'https://sweet-shop-api-z9ci.onrender.com/api',
-  SUPABASE_URL: 'https://nsraqofbnfmbtvzcaleq.supabase.co',
-  SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5zcmFxb2ZibmZtYnR2emNhbGVxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAyNTYxMjksImV4cCI6MjA4NTgzMjEyOX0.mopW-W4s0vc41nqWGefCi-Ich4pVgAIlOTu_RpqgTac'
-};
